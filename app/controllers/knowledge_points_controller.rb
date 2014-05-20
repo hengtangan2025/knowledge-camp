9a1ff4771623849f9d3809019e89e6343e1ac494
fushang318 <fushang318@gmail.com>
2014-05-20 18:16:58 +0800

    add knowleget curd page

--- app/controllers/knowledge_points_controller.rb
+++ app/controllers/knowledge_points_controller.rb
@@ -9,5 +9,35 @@ class KnowledgePointsController < ApplicationController
   end
 
   def create
+    @point = @net.points.build(_point_params)
+    if @point.save
+      return redirect_to "/knowledge_nets/#{@net.id}"
+    end
+    redirect_to :action => :new
+  end
+
+  def show
+    @point = KnowledgeNetStore::Point.find(params[:id])
+  end
+
+  def edit
+    @point = KnowledgeNetStore::Point.find(params[:id])
+  end
+
+  def update
+    @point = KnowledgeNetStore::Point.find(params[:id])
+    @point.update_attributes(_point_params)
+    return redirect_to "/knowledge_nets/#{@net.id}"
+  end
+
+  def destroy
+    @point = KnowledgeNetStore::Point.find(params[:id])
+    @point.destroy
+    redirect_to "/knowledge_nets/#{@net.id}"
+  end
+
+  private
+  def _point_params
+    params.require(:knowledge_net_store_point).permit(:name, :desc, :parent_ids => [])
   end
 end
