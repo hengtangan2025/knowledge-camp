976938afd243cffe95b3abf23496d9c414ca4057
Kaid <kaid@kaid.me>
2014-05-21 18:28:53 +0800

    added style

--- app/helpers/plan_store_helper.rb
+++ app/helpers/plan_store_helper.rb
@@ -7,12 +7,12 @@ module PlanStoreHelper
     }
 
 
-    render :partial => "/partials/plan_store_form",
+    render :partial => "/plan_store_partials/form",
            :locals  => locals
   end
 
-  def delete_link(text, href)
+  def delete_link(text, href, klass: "")
     data = {:confirm => "确定删除?"}
-    link_to text, href, :method => :delete, :data => data
+    link_to text, href, :method => :delete, :data => data, :class => klass
   end
 end
