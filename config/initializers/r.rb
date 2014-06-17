e1b7eec9eaccafbd1aa681fe6ae3a3d1c4cc6e2a
fushang318 <fushang318@gmail.com>
2014-06-17 10:47:37 +0800

    fix

--- config/initializers/r.rb
+++ config/initializers/r.rb
@@ -1,12 +1,14 @@
-HASH = {
-  :static_file_base_path_prefix => "public"
-}
+if !defined? R
+  HASH = {
+    :static_file_base_path_prefix => "public"
+  }
 
-class R
-  STATIC_FILE_URL_PREFIX = "static_files"
+  class R
+    STATIC_FILE_URL_PREFIX = "static_files"
 
-  UPLOAD_FILE_BASE_PATH = File.join(
-    File.expand_path(HASH[:static_file_base_path_prefix], Rails.root),
-      STATIC_FILE_URL_PREFIX    
-    )
+    UPLOAD_FILE_BASE_PATH = File.join(
+      File.expand_path(HASH[:static_file_base_path_prefix], Rails.root),
+        STATIC_FILE_URL_PREFIX    
+      )
+  end
 end
