5a546ffed2c6b825be5815b01953b3f405d14013
ben7th <ben7th@sina.com>
2014-07-25 18:37:52 +0800

    fix engine asset path

--- vendor/explore/app/helpers/explore/application_helper.rb
+++ vendor/explore/app/helpers/explore/application_helper.rb
@@ -1,11 +1,11 @@
 module Explore
   module ApplicationHelper
-    def tutorials_mock(view)
+    def tutorials_mock()
       @tutorials = [
         OpenStruct.new({
           :net => KnowledgeNetStore::Net.first,
           :id => 'sample-1',
-          :img => view.asset_path('/assets/sample/shao.jpg'),
+          :img => 'http://oss.aliyuncs.com/pie-documents/20140725/shao.jpg',
           :title => '勺工基本技法入门',
           :desc => '勺工是中式烹调特有的一项技术，是中式烹调用火和施艺的独特功夫。运用勺工技艺，调节和控制火候是每个厨师必备的基本功之一。',
           :steps => [
@@ -171,7 +171,7 @@ module Explore
         OpenStruct.new({
           :net => KnowledgeNetStore::Net.first,
           :id => 'sample-2',
-          :img => view.asset_path('/assets/sample/dao.jpg'),
+          :img => 'http://oss.aliyuncs.com/pie-documents/20140725/dao.jpg',
           :title => '刀工基本技法入门',
           :desc => '刀工就是根据烹调与食用的需要，将各种原料加工成一定形状，使之成为组配菜肴所需要的基本形体的操作技术。'
         }),
