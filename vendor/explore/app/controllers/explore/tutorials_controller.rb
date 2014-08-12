7a12423d3a6f344c5447dadcc6c8abd0695a93c0
ben7th <ben7th@sina.com>
2014-08-12 18:46:15 +0800

    更新演示数据，增加美术知识网

--- vendor/explore/app/controllers/explore/tutorials_controller.rb
+++ vendor/explore/app/controllers/explore/tutorials_controller.rb
@@ -2,17 +2,14 @@ module Explore
   class TutorialsController < ApplicationController
     layout 'explore/tutorial', :only => [:show]
 
-    def index
-      @net = KnowledgeNetStore::Net.find params[:net_id]
-      redirect_to @net
-    end
-
     def show
       @tutorials = Explore::Mock.tutorials
       @tutorial = @tutorials.select {|x| x.id.to_s == params[:id]}.first
       @parents = @tutorial.parents.map {|pid|
         @tutorials.select {|x| x.id.to_s == pid}.first
       }
+
+      @net = Explore::Mock.nets.select {|x| x.id.to_s == @tutorial.net_id}.first
     end
   end
 end
