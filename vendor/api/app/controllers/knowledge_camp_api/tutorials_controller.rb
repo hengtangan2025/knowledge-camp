228e969bfbbf0fbff444b20a607b71edf80ee11c
Kaid <kaid@kaid.me>
2014-08-21 13:09:56 +0800

    added related logic

--- vendor/api/app/controllers/knowledge_camp_api/tutorials_controller.rb
+++ vendor/api/app/controllers/knowledge_camp_api/tutorials_controller.rb
@@ -17,6 +17,8 @@ module KnowledgeCampApi
         net.plans.map(&:topics).flatten.map(&:tutorials).flatten
       when :point_id
         KnowledgeNetStore::Point.find(params[:point_id]).tutorials
+      when :topic_id
+        KnowledgeNetPlanStore::Topic.find(params[:topic_id]).tutorials
       when :ancestor_id
         base.try :descendants
       when :parent_id
