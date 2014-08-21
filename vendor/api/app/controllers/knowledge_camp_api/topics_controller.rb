2db7d6ab37198a71c3a14b20fc8c24b16a40bbf0
Kaid <kaid@kaid.me>
2014-08-21 11:00:41 +0800

    fixed a typo

--- vendor/api/app/controllers/knowledge_camp_api/topics_controller.rb
+++ vendor/api/app/controllers/knowledge_camp_api/topics_controller.rb
@@ -1,7 +1,7 @@
 module KnowledgeCampApi
   class TopicsController < ApplicationController
     def index
-      plan_id = params(:plan_id)
+      plan_id = params[:plan_id]
 
       if plan_id
         display KnowledgeNetPlanStore::Topic.where(:plan_id => plan_id)
