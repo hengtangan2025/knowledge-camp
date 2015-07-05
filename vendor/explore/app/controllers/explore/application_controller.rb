ee537d02a942131e62bb69065b79903cf7b9cf0f
ben7th <ben7th@sina.com>
2015-07-06 05:19:53 +0800

    bugfix

--- vendor/explore/app/controllers/explore/application_controller.rb
+++ vendor/explore/app/controllers/explore/application_controller.rb
@@ -4,7 +4,10 @@ module Explore
       ua = request.user_agent.to_s.downcase
       return true if ua =~ /iphone/
       return true if ua =~ /android/
+
+      sign_in User.first
       return false
+      # return true
     end
   end
 end
