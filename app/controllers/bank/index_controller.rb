a11202324aa63e9eb86e17dba9dc8d507ad250c3
destinyd <destinyd_war@163.com>
2015-11-17 15:52:43 +0800

    主页：是否登录决定不同的显示

--- app/controllers/bank/index_controller.rb
+++ app/controllers/bank/index_controller.rb
@@ -1,11 +1,8 @@
 class Bank::IndexController < Bank::ApplicationController
   def index
-    @learning_courses = current_user.courses.page.per(2)
-
+    @learning_courses = current_user ? current_user.courses.page.per(2) : []
     @recommend_courses = KcCourses::Course.page
-
     @recent_courses = KcCourses::Course.page.per(10)
-
     @hot_courses = KcCourses::Course.page.per(10)
   end
 end
