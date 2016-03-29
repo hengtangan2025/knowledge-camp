e5e1a8f197b8b5b5557f40624477b4d7165ebedb
ben7th <ben7th@sina.com>
2016-03-29 10:33:54 +0800

    split routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
-Rails.application.routes.draw do
+def routes_draw(routes_name)
+  instance_eval(File.read(Rails.root.join('config', "routes_#{routes_name}.rb")))
+end
 
+routes_draw :mockup
+
+Rails.application.routes.draw do
   # api
   mount KnowledgeCampApi::Engine => '/api'
 
@@ -165,19 +170,4 @@ Rails.application.routes.draw do
       resources :model_labels
     end
   end
-
-  # ------------------
-  # kc mobile 2016 mockup
-  get     '/mockup/:page' => 'mockup#page', as: 'mockup'
-  post    '/mockup/:req'  => 'mockup#do_post', as: 'mockup_post'
-  delete  '/mockup/:req'  => 'mockup#do_delete', as: 'mockup_delete'
-
-  get     '/mockup/auth/:page' => 'mockup#auth_page', as: 'mockup_auth'
-  post    '/mockup/auth/:req'  => 'mockup#auth_do_post', as: 'mockup_auth_post'
-  delete  '/mockup/auth/:req'  => 'mockup#auth_do_delete', as: 'mockup_auth_delete'
-
-  get     '/mockup/manager/:page' => 'mockup#manager_page', as: 'mockup_manager'
-  post    '/mockup/manager/:req'  => 'mockup#manager_do_post', as: 'mockup_manager_post'
-  put     '/mockup/manager/:req'  => 'mockup#manager_do_put', as: 'mockup_manager_put'
-  delete  '/mockup/manager/:req'  => 'mockup#manager_do_delete', as: 'mockup_manager_delete'
 end
